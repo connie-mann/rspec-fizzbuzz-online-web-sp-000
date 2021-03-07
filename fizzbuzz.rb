e42c1949bda461ad35b9f397a9a22e73c59e6a5b
Constance Mann <connie.mann@yahoo.com>
2021-03-07 19:21:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-  puts "Fizz"
+
+def fizzbuzz(int)
+  if int % 3 == 0
+    "Fizz"
+  end
 end
 
-if x/3
-  puts "Fizz"
-end
+  
