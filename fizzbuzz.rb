504e5ca3aa110f81f39f11fcb18d9e9bab1aee1b
Constance Mann <connie.mann@yahoo.com>
2021-03-07 21:37:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,10 +4,13 @@
 def fizzbuzz(int)
   if int % 3 == 0
       "Fizz"
+  end
   if int % 5 == 0
     "Buzz"
+  end
   if int % 3 == 0 && int % 5 == 0
     "Fizzbuzz"
+  end
   else
     puts int
   end
